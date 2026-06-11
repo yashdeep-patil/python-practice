3f395673e2a0aeb581e86b844b810b2fc557d284
Yashdeep-patil <yashdeeppatil98@gmail.com>
2026-06-11 22:51:27 +0530

    Added Oop, File Handling, Error Handling

--- Error_handling/voting_Eligibility.py
+++ Error_handling/voting_Eligibility.py
@@ -10,14 +10,14 @@ def check_voting_eligibility(age):
     elif age < 18:
         raise AgeError("Not eligible to vote.")
     else:
-        return "Eligible to vote."
-    
-
+        return "Eligible to vote."   
 try:
     print(check_voting_eligibility(20))
     print(check_voting_eligibility(15))  
     print(check_voting_eligibility(-5))   
-      
+
 except AgeError as e:
     print(e)
 
+finally:
+    print("Voting eligibility check completed.")
